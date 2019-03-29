7cae8a7ea260a1d341c1dc06aa230cf8cac0b1f8
Jure Čular <jure.cular@microblink.com>
2019-03-29 09:58:05 +0100

    Added validator for combined recognizer and non combined recognizers

---
+++ BlinkIDUI/BlinkIDUI/Scanning/ResultValidator/MBCombinedValidator.swift
@@ -0,0 +1,22 @@
+//
+//  MBCombinedValidator.swift
+//  BlinkIDUI
+//
+//  Created by Jure Čular on 3/29/19.
+//  Copyright © 2019 Microblink. All rights reserved.
+//
+
+import MicroBlink
+
+class MBCombinedValidator: MBValidatorProtocol {
+
+    func match(results: [MBFieldResult]) -> Bool {
+        guard let combinedRecognizer = results.first as? MBCombinedRecognizer else {
+            // TODO: think about this
+            return true
+        }
+
+        return combinedRecognizer.combinedResult.documentDataMatch
+    }
+
+}
