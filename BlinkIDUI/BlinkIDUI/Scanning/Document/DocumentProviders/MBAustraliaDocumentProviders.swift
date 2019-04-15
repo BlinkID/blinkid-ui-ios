8a7618b86e187f173174cab7f25760f4084f13a8
Jura Skrlec <jura.skrlec@microblink.com>
2019-04-15 11:11:22 +0200

    Updated strings

---
+++ BlinkIDUI/BlinkIDUI/Scanning/Document/DocumentProviders/MBAustraliaDocumentProviders.swift
@@ -0,0 +1,9 @@
+//
+//  MBAustraliaDocumentProviders.swift
+//  BlinkIDUI
+//
+//  Created by Jura Skrlec on 15/04/2019.
+//  Copyright © 2019 Microblink. All rights reserved.
+//
+
+import Foundation
